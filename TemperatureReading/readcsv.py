8ab770e1a17d64ff0ff411ff1cdc2847e97e7db8
Andrew Yanez <andrewyanez@ucsb.edu>
2023-07-07 16:17:17 -0700

    all

--- TemperatureReading/readcsv.py
+++ TemperatureReading/readcsv.py
@@ -3,37 +3,43 @@ import matplotlib.animation as animation
 from matplotlib import style
 import datetime
 
+def read_data(filename):
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                try:
+                    timestamp, value = line.split(',')
+                    yield timestamp[10:19], float(value)
+                except (ValueError, IndexError):
+                    continue
+
 def animate(i):
-    graph_data = open('data.csv', 'r').read()
-    lines = graph_data.split('\n')
     xs = []
     ys = []
-    for line in lines:
-        if len(line) > 1:
-            x, y = line.split(',')
-            try:
-                xs.append(x[10:19])
-                ys.append(float(y))
-            except ValueError:
-                continue
+    for timestamp, value in read_data('data.csv'):
+        xs.append(timestamp)
+        ys.append(value)
     
-    ax1.clear()
-    # Set the figure size
-    ax1.plot(xs, ys, '-o')
-    ax1.set_xlabel('Time')
-    ax1.set_ylabel('Temp (K)')
+    ax.clear()
+    ax.plot(xs, ys, '-o')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Temp (K)')
     date_str = datetime.datetime.now().strftime('%B %d')
-    ax1.text(0.95, 0.95, date_str, transform=ax1.transAxes,
+    ax.text(0.95, 0.95, date_str, transform=ax.transAxes,
              horizontalalignment='right', verticalalignment='top', fontsize=12)
-
-
+    
+    # Limit the number of x-values shown per time
+    tick_frequency = 1  # Show a tick every 5 data points
+    ax.set_xticks(ax.get_xticks()[::tick_frequency])
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(90)
 
 if __name__ == "__main__":
     #style.use('fivethirtyeight')
 
     fig = plt.figure()
-    ax1 = fig.add_subplot(1, 1, 1)
+    ax = fig.add_subplot(1, 1, 1)
     
-
     ani = animation.FuncAnimation(fig, animate, interval=1000)
     plt.show()
