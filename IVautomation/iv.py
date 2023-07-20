a9c62afb8dd8aeb3ea8f41c41257a4c01e2f2841
Andrew Yanez <andrewyanez@ucsb.edu>
2023-07-20 15:45:28 -0700

    test

--- IVautomation/iv.py
+++ IVautomation/iv.py
@@ -2,12 +2,9 @@ import pyvisa as pv
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
 
-def func(x, m, c):
-    return m * x + c
 
-def single_IV_sweep(keysight=None, channel=1, start=0, stop=10, points=10, aper=1E-4, current_compliance=0.1):
+def single_IV_sweep(keysight=None, channel=1, start=0, stop=10, points=10, current_compliance=0.1):
     '''
         Remember to connect, initialze keysight and import numpy before executing this function!
         Runs an IV measurement on Channel 1.
@@ -16,41 +13,27 @@ def single_IV_sweep(keysight=None, channel=1, start=0, stop=10, points=10, aper=
 
     # Source
     keysight.write("*RST")
-    #keysight.write(":SOUR:FUNC:MODE VOLT")
+    keysight.write(":SOUR:FUNC:MODE VOLT")
     keysight.write(":SOUR:VOLT:MODE SWE")
-    #keysight.write(":SOUR:VOLT:RANG:AUTO:LLIM 0.002")
     keysight.write(":SOUR:VOLT:STAR " + str(start))
     keysight.write(":SOUR:VOLT:STOP " + str(stop))
     keysight.write(":SOUR:VOLT:POIN " + str(points))
 
     # Sense
     keysight.write(":SENSE:FUNC ""CURR""")
-    #keysight.write(":SENSE:CURR:RANG:AUTO OFF")
-    #keysight.write(":SENSE:CURR:RANG 1E-9")
-    keysight.write(":SENSE:CURR:APER " + str(aper))
+    keysight.write(":SENSE:CURR:APER 1e-4")
     keysight.write(":SENSE:CURR:PROT " + str(current_compliance))
-    # keysight.write(":SENSE:CURR:RANG:AUTO:LLIM 1E-9") # 1nA
 
-    # Trigger
+    # Automatic Trigger
     keysight.write(":TRIG:SOUR AINT")
     keysight.write(":TRIG:COUN " + str(points))
-
 
     # measurement
     keysight.write(":OUTP" + str(channel) + " ON")
     keysight.write(":INIT (@" + str(channel) + ")")
     keysight.write(":FETC:ARR:CURR? (@" + str(channel) + ")")
     data = keysight.read()
-    '''
-    #ioObj.WriteString(":#SOUR:VOLT:MODE LIST")
-    #ioObj.WriteString(":SOUR:LIST:VOLT 0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1")
-
-    keysight.write(":DISP:ENAB ON")
-    keysight.write(":DISP:VIEW GRAP")
-    keysight.write(":HCOP:SDUM:FORM JPG")
-    keysight.write("*OPC?") 
-    keysight.write(":HCOP:SDUM:DATA?")
-    '''
+    print("data: ", data)
     keysight.write(":OUTP" + str(channel) + " OFF")
 
 
@@ -62,7 +45,23 @@ def single_IV_sweep(keysight=None, channel=1, start=0, stop=10, points=10, aper=
 
     return current_list
 
-
+def intialize_device():
+    keysight_usb_id = 'USB0::0x0957::0x8C18::MY51145486::INSTR'
+    rm = pv.ResourceManager()
+    print(rm)
+    print(rm.list_resources())
+    try:
+        keysight = rm.open_resource(keysight_usb_id) # open Keysight according to the usb id of keysight that comes along with it.
+    except:
+        print("Failed to connect to Keysight. Please check your connection")
+        exit(1)
+    '''
+    code for testing if keysight is connected successfully
+    '''
+    print(keysight)
+    print(keysight.query('*IDN?')) # return ID information
+    keysight.write('*RST') # to reset all setup on the keysight
+    time.sleep(0.1)
 
 
 
@@ -92,15 +91,17 @@ time.sleep(0.1)
 
 
 # initializing the parameters
-start = 0
+start = -1
 stop = 1
 points = 5
 save_file = False
 
+# Return evenly spaced numbers over a specified interval.
 volt_list = np.linspace(start, stop, points)
 M = np.zeros((10, points))
 for i in range(10):
-    M[i] = single_IV_sweep(keysight, 1, start, stop, points, aper=0.005, current_compliance=2e-4) #2e-4 for a 56k Ohm Resistor
+    M[i] = single_IV_sweep(keysight, 1, start, stop, points, current_compliance=2e-4)
+print(M)
 
 # data = single_IV_sweep(keysight,1, start,stop,points, nplc=0.2, current_compliance=10e-9)
 # m = np.zeros((2, points), dtype=np.float64) # m is the matrix where the first row is the voltage, and the second row is corresponding current
@@ -118,27 +119,12 @@ if save_file:
     np.savetxt(filename, m, delimiter=',')
 
 keysight.close()
-
-x = list(np.linspace(start, stop, points))
-y = list(np.mean(M, axis=0))
-
-params, covariance = curve_fit(func, x, y)
-m_fit, c_fit = params
-slope = m_fit
-print("Slope: ", slope)
-
-x_fit = np.linspace(min(x), max(x), 100)
-y_fit = func(x_fit, m_fit, c_fit)
-plt.scatter(x, y, s=6)
-slope_text = f"Slope: {slope}"
-
-plt.plot(x_fit, y_fit, linestyle='--', color='red', label=f'Fitted Line ({slope_text})')
-
-print("volts",x)
-print("amps",y)
-
+plt.scatter(np.linspace(start, stop, points), np.mean(M, axis=0), s=6)
 plt.title("Diode IV")
-plt.xlabel('Voltage(V)')
-plt.ylabel('Current(Amps?)')
-plt.legend()
 plt.show()
+# print(np.mean(M, axis=0))
+# print(np.mean(M, axis=0).shape)
+# plt.scatter(np.linspace(start, stop, points), data - np.linspace(start, stop,points) * para[0] - para[1])
+# plt.show()
+#
+# print("Noise level: ", np.std(data - np.linspace(start, stop,points) * para[0] - para[1]))
