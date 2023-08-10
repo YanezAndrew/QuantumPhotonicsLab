cb185e6d5a3c7020f82c9b24d45ac82ba65e93e8
Andrew Yanez <andrewyanez@ucsb.edu>
2023-08-09 18:11:34 -0700

    SD

--- TemperatureReading/iv_plot_over.py
+++ TemperatureReading/iv_plot_over.py
@@ -4,10 +4,6 @@ import os
 import pandas as pd
 
 # Sample data extracted from the provided line
-voltage = [-8.0, -7.111111111111111, -6.222222222222222, -5.333333333333334, -4.444444444444445, -3.5555555555555554, -2.666666666666667, -1.7777777777777786, -0.8888888888888893, 0.0]
-current = [-0.00014354539999999997, -0.00012759909999999998, -0.00011164979999999999, -9.570410000000001e-05, -7.973751999999998e-05, -6.379562e-05, -4.7848239999999996e-05, -3.190002e-05, -1.595105e-05, 2.2000000000000003e-09]
-
-
 if __name__ == "__main__":
     #style.use('fivethirtyeight')
     current_dir = os.getcwd()
@@ -40,8 +36,15 @@ if __name__ == "__main__":
         file_path = os.path.join(current_dir, file_name)
         print(file_path)
         df = pd.read_csv(file_path)
+        voltage = list(df['Voltage'])
+        current = list(df['Amps'])
+        print(type(voltage))
+        print(voltage)
+        print(current[0])
         plt.figure()
-        plt.plot(voltage, current, marker='o', linestyle='-', color='b')
+        for i in range(len(voltage)):
+            for j in range(len(voltage[0])):
+                plt.scatter(voltage[i][j], current[i][j], marker='o', linestyle='-', color='b')
         plt.xlabel('Voltage')
         plt.ylabel('Current')
         plt.title('Current vs. Voltage')
