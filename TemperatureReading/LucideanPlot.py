4c8b44c4353d0027afa1035102b06ef804d29fe8
Andrew Yanez <andrewyanez@ucsb.edu>
2023-08-16 19:19:36 -0700

    7:19 got the color gradient code to work

--- TemperatureReading/LucideanPlot.py
+++ TemperatureReading/LucideanPlot.py
@@ -2,33 +2,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import ast
 import numpy as np
-from matplotlib.colors import LogNorm 
 from matplotlib.colors import LinearSegmentedColormap
-
 
 df = pd.read_csv('data_2023-08-10 (50) - Lucidean.csv')
 df['Voltage'] = df['Voltage'].apply(ast.literal_eval)
 df['Amps'] = df['Amps'].apply(ast.literal_eval)
 xvals = np.array(df['Voltage'].values[0])
-
-
-
 plt.figure(dpi=150)
 
-colors = [(1, 1, 0), (0, 0, 0.5)]  # Yellow to Dark Blue
-cmap_name = 'yellow_to_dark_blue'
+# Create a custom colormap with increased intensity for negative slopes
+colors = [(1, 1, 0), (0, 0, 1)]  # Yellow to Blue
+cmap_name = 'yellow_to_blue'
 cm = LinearSegmentedColormap.from_list(cmap_name, colors)
-num_curves = len(df['Amps'])
+plt.xlim(0, 1.5)
+plt.ylim(1e-14, 10e-3)
+prev = df['Temperature'][0]
+num_curves = len(df['Temperature'])
+add = 0.000
+max_temp = df['Temperature'][0]
+for cnt, i in enumerate(df['Temperature']):
+    if cnt == 0:
+        dx = 0
+    else:
+        dx = prev - i
+        prev = i
+        add += dx
+        color = cm(add / max_temp)
+        #print(color)
+        plt.semilogy(xvals, np.abs(df['Amps'][cnt]), color=color, label=f'Curve {i + 1}')
+        #plt.legend()
+        #plt.pause(0.000001)
 
-for i, current_data in enumerate(df['Amps']):
-    color = cm(i / (num_curves - 1)) # Determine color based on the position of the curve
-    for value in current_data:
-        if abs(value) < 1e-17:
-            print(f"Curve {i} has a value less than 1e-17: {value}")
-            print(df['Time'][i])
-    plt.semilogy(xvals, np.abs(current_data), color=color, label=f'Curve {i + 1}')
-#plt.xlim(0, 1.5)
-#plt.ylim(1e-14, 10e-3)
-plt.xlabel('Voltage [V]')
-plt.ylabel('Current [Log(A)]')
 plt.show()
