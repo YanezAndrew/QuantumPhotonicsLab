b16203dcb341c7a3ab8a7f5d3a6fa9915e71f44c
Andrew Yanez <andrewyanez@ucsb.edu>
2023-08-23 12:50:23 -0700

    finer

--- TemperatureReading/LucideanPlot.py
+++ TemperatureReading/LucideanPlot.py
@@ -5,55 +5,61 @@ import numpy as np
 from matplotlib.colors import LinearSegmentedColormap
 from matplotlib.colors import ListedColormap
 import time
+from decimal import Decimal
 start_time = time.time()
 # 'data_2023-08-10 (50) - Lucidean.csv'
-df = pd.read_csv('data_2023-08-18 (5) - Lucidean_2.csv')
+# 'data_2023-08-18 (5) - Lucidean_2.csv'
+# 'data_2023-08-22 (4) - Lucidean_2nDevice.csv'
+
+df = pd.read_csv('data_2023-08-22 (4) - Lucidean_2nDevice.csv')
+df = df.sort_values(by='Temperature', ascending=False)
+df.reset_index(drop=True, inplace=True)
+#df = df[:538]
 df['Voltage'] = df['Voltage'].apply(ast.literal_eval)
 df['Amps'] = df['Amps'].apply(ast.literal_eval)
+
 xvals = np.array(df['Voltage'].values[0])
 fig, ax = plt.subplots(figsize=(16, 9))
-fig.suptitle("Lucidean EOM Temperature IV")
+fig.suptitle("Lucidean EOM Temperature IV - Device #2")
 
-# Create a custom colormap with increased intensity for negative slopes
-#colors = [(1, 1, 0), (0, 0, 1)]  # Yellow to Blue
-#cmap_name = 'yellow_to_blue'
 cm = plt.cm.get_cmap("jet").reversed()
 plt.xlim(0, 1.5)
 plt.ylim(1e-14, 5e-3)
+
 prev = df['Temperature'][0]
 num_curves = len(df['Temperature'])
-add = 0.000
-max_temp = df['Temperature'][0]
+add = 0.0
+max_temp = max(df['Temperature'])
 min_temp = min(df['Temperature'])
+range = round(max_temp-min_temp, 3)
 
-#print(min_temp)
-#cbar_ax = plt.subplot(1, 2, 2)
 unique_ordered_elements = []
 unique_ordered_temp = []
-
+test_cnt = 0
 for cnt, i in enumerate(df['Temperature']):
     if cnt == 0:
-        dx = 0
-        plt.semilogy(xvals, np.abs(df['Amps'][0]), color=cm(0), label=f'Curve {i + 1}')
+        prev_color = cm(0.0)
+        dx = 0.0
+        plt.semilogy(xvals, np.abs(df['Amps'][0]), color=cm(dx), label=f'Curve {i + 1}')
     else:
-        dx = prev - i
+        dx = round(prev - i, 3)
         prev = i
-        add += dx
-        color = cm(add / (max_temp-min_temp))
-        #rint(add/max_temp)
-        #print("ADD: ", add)
-        if color not in unique_ordered_elements:
-            unique_ordered_elements.append(color)
-            unique_ordered_temp.append(i)
-        #print(color)
-        plt.semilogy(xvals, np.abs(df['Amps'][cnt]), color=color, label=f'Curve {i + 1}')
+        add = round(add + dx, 3)
+        color = cm(add / range)
+        if dx > 0.0:
+            if (prev_color != color):
+                unique_ordered_elements.append(color)
+                unique_ordered_temp.append(i)
+                plt.semilogy(xvals, np.abs(df['Amps'][cnt]), color=color, label=f'Curve {i + 1}')
+                test_cnt +=1
+            prev_color = color
         #plt.pause(0.000001)
-
 # Add a color bar
+print(test_cnt)
 cmap = ListedColormap(unique_ordered_elements).reversed()
-normalize = plt.Normalize(min(unique_ordered_temp), max(unique_ordered_temp))
+normalize = plt.Normalize(max_temp, min_temp)
 sm = plt.cm.ScalarMappable(cmap=cmap, norm = normalize)
-print(min(unique_ordered_temp))
+print("Last Temp Before Color Doesn't Change Anymore:", min(unique_ordered_temp))
 
 # # Choose a subset of temperature ticks to display on the color bar
 num_ticks = 5  # Number of ticks to display
